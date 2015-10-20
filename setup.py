7c11b56c4b77b7e77abbc8884fca78cb23fbe466
abbse <aleix.badia@somenergia.coop>
2015-10-20 20:11:09 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+INSTALL_REQUIRES = [
+]
+
+setup(
+    name='ramman',
+    version='0.1.1',
+    packages=['ramman'],
+    url='http://www.somenergia.coop',
+    license='MIT',
+    install_requires=INSTALL_REQUIRES,
+    entry_points="""
+    """,
+    author='Somenergia, coop',
+    author_email='',
+    description='Heman tiny client'
+)
---
+++ tests/test_req.py
@@ -0,0 +1,46 @@
+import os
+import csv
+import unittest
+from ramman import Ramman
+
+
+def read_from_file(filename):
+    entries = []
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f, delimiter=';')
+        entries = [row for row in reader]
+    return entries
+
+
+class HemanResultsTest(unittest.TestCase):
+    client = None
+
+    @classmethod
+    def setUpClass(self):
+        config = {
+            'url': os.getenv('HEMAN_URL', None),
+            }
+        self.client = Ramman(config)
+
+    def test_ot101_1(self):
+        attrs = [u'diffAverageConsumption',
+                 u'criteriaSelected',
+                 u'numberCustomersEff',
+                 u'consumption',
+                 u'consumptionNumberOfDays',
+                 u'averageEffConsumption',
+                 u'month',
+                 u'numberCustomers',
+                 u'averageConsumption',
+                 u'diffAverageEffConsumption',
+                 u'contractId']
+
+        contract = read_from_file('data/test_ot101_1.csv')[0]
+
+        results = self.client.get_results_by_contract('OT101', contract[0], contract[1], contract[2])
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].keys(), attrs)
+
+if __name__ == '__main__':
+    unittest.main()
